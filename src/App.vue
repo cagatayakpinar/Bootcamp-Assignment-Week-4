9711851253d1d3de4c3f7ba4cdeefc6ae3c4ac17
Çağatay Akpınar <63678992+cagatayakpinar@users.noreply.github.com>
2022-03-06 07:35:24 +0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="page-top">
+    <FooterPage> </FooterPage>
+    <LogOut> </LogOut>
+    <NavAlert> </NavAlert>
+    <NavMessage> </NavMessage>
+    <NavSearch> </NavSearch>
+    <SidebarChats> </SidebarChats>
+    <SidebarDashboard> </SidebarDashboard>
+    <SidebarHeadings> </SidebarHeadings>
+    <SidebarPagesCollapse> </SidebarPagesCollapse>
+    <SidebarTables> </SidebarTables>
+    <SidebarToggler> </SidebarToggler>
+    <SidebarUtilities> </SidebarUtilities>
+    <NavTopbar> </NavTopbar>
+    <UserInfo> </UserInfo>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterPage from "./components/FooterPage.vue";
+import LogOut from "./components/LogOut.vue";
+import NavAlert from "./components/NavAlert.vue";
+import NavMessage from "./components/NavMessage.vue";
+import NavSearch from "./components/NavSearch.vue";
+import SidebarChats from "./components/NavBar/SidebarChats.vue";
+import SidebarDashboard from "./components/SidebarDashboard.vue";
+import SidebarHeadings from "./components/SidebarHeadings.vue";
+import SidebarPagesCollapse from "./components/SidebarPagesCollapse.vue";
+import SidebarTables from "./components/SidebarTables.vue";
+import SidebarToggler from "./components/SidebarToggler.vue";
+import SidebarUtilities from "./components/SidebarUtilities.vue";
+import NavTopbar from "./components/NavTopbar.vue";
+import UserInfo from "./components/UserInfo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FooterPage,
+    LogOut,
+    NavAlert,
+    NavMessage,
+    NavSearch,
+    SidebarChats,
+    SidebarDashboard,
+    SidebarHeadings,
+    SidebarPagesCollapse,
+    SidebarTables,
+    SidebarToggler,
+    SidebarUtilities,
+    NavTopbar,
+    UserInfo,
+  },
+};
 </script>
 
 <style>
